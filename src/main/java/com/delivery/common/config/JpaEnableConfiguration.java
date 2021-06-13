71739dfe416a4da3852d7b686a53eae7cdf30675
0hun <whdudgns2654@gmail.com>
2021-06-13 18:43:57 +0900

    refactor : JpaEnableConfiguration 패키지 변경
    JpaEnableConfiguration 패키지 변경
    - EnableJpaAuditing 기능은 공통 기능이므로 common 패키지로 변경

--- src/main/java/com/delivery/common/config/JpaEnableConfiguration.java
+++ src/main/java/com/delivery/common/config/JpaEnableConfiguration.java
@@ -1,4 +1,4 @@
-package com.delivery.config;
+package com.delivery.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
