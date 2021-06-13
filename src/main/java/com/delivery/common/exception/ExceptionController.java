0db67b22c50be647409e016247ba58271511a1b3
0hun <whdudgns2654@gmail.com>
2021-06-13 22:17:21 +0900

    refactor : ExceptionController error message 리턴
    ExceptionController error message 리턴
    - 에러 리턴시 이전 Exception에서 message를 가져와서 body에 리턴하도록 수정

--- src/main/java/com/delivery/common/exception/ExceptionController.java
+++ src/main/java/com/delivery/common/exception/ExceptionController.java
@@ -2,6 +2,7 @@ package com.delivery.common.exception;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -10,29 +11,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- *  @RestControllerAdvice : 모든 컨트롤러에서 발생하는 예외를 잡아 처리해주는 어노테이션(RestBody + ControllerAdvice)
+ * @RestControllerAdvice : 모든 컨트롤러에서 발생하는 예외를 잡아 처리해주는 어노테이션(RestBody + ControllerAdvice)
  */
 @RestControllerAdvice
 public class ExceptionController {
 
-  /**
-   * @valid 유효성 체크에 통과하지 못하면 MethodArgumentNotValidException 발생한다.
-   * @param bindingResult
-   * @return ResponseEntity<List<ObjectError>>
-   */
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<List<ObjectError>> methodValidException(BindingResult bindingResult) {
-    return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
-  }
+    /**
+     * @param bindingResult
+     * @return ResponseEntity<List < ObjectError>>
+     * @valid 유효성 체크에 통과하지 못하면 MethodArgumentNotValidException 발생한다.
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ObjectError>> methodValidException(BindingResult bindingResult) {
+        return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+    }
 
-  /**
-   * repository에서 값을 찾지 못할 경우 NoSuchElementException 발생한다.
-   * 에러를 잡아 404 code return
-   * @return ResponseEntity<Void>
-   */
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<Void> noSuchElementException() {
-    return ResponseEntity.notFound().build();
-  }
+    /**
+     * repository에서 값을 찾지 못할 경우 NoSuchElementException 발생한다.
+     * NoSuchElementException 발생시 에러 메세지와 함께 http 404 code return
+     *
+     * @return ResponseEntity<String>
+     */
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> noSuchElementException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
 
 }
